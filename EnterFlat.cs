63cd99a6ba579ece2468ef30aa8b0f0e55e41f09
ASLaneZero <mail@aslane.co.uk>
2023-05-04 13:06:42 +0100

    Mailflag change

--- EnterFlat.cs
+++ EnterFlat.cs
@@ -15,13 +15,13 @@ namespace LittleDenham
         }
         internal static void FlatMessage(string tileActionString, Vector2 position)
         {
-                if (!Game1.player.mailReceived.Contains("OpenedFlat2A"))
-                {
+                if (!Game1.player.hasOrWillReceiveMail("OpenedFlat2A"))
+            {
                     Game1.playSound("woodWhack");
                     Game1.drawObjectDialogue(Game1.parseText(Helper.Translation.Get("LD.FlatA.Door")));
-                    Game1.player.mailReceived.Add("OpenedFlat2A");
+                    Game1.player.mailForTomorrow.Add("OpenedFlat2A");
                 }
-                else if (Game1.player.mailReceived.Contains("OpenedFlat2A"))
+                else if (Game1.player.hasOrWillReceiveMail("OpenedFlat2A"))
                 {
                     Game1.playSound("doorClose");
                     Game1.warpFarmer("Custom_LDFlat2A", 8, 13, 0);
